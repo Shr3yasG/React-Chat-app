ce198fd69b3e6990eecb00a01dd2be5831ff737e
Shreyas Gawade <shreyaa1110@gmail.com>
2024-07-21 18:41:15 +0530

    Initial commit

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "react-chat-app-ad0f5.firebaseapp.com",
+  projectId: "react-chat-app-ad0f5",
+  storageBucket: "react-chat-app-ad0f5.appspot.com",
+  messagingSenderId: "713165781880",
+  appId: "1:713165781880:web:1b39a8b2218c5b9edee68d"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
+export const db = getFirestore()
+export const storage = getStorage()
